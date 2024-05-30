8b5482304348da738bbf9a4467fa40bdac15d38e
Rubén Zúñiga <144453288+Ruben-Zuniga@users.noreply.github.com>
2024-05-30 16:20:40 -0300

    Modificada la escala de tiempo y la instanciacion del modulo

--- tb/eth_phy_10g_tb_LL1R.v
+++ tb/eth_phy_10g_tb_LL1R.v
@@ -1,22 +1,25 @@
-`timescale 1ns / 1ps
 `include "eth_phy_10g.v"
+
+`resetall
+`timescale 1us / 100ns
+`default_nettype none
 
 module eth_phy_10g_LL1R;
 
-    // Parámetros del módulo
+    // Parametros del modulo
     parameter DATA_WIDTH = 64;
     parameter CTRL_WIDTH = (DATA_WIDTH/8);
     parameter HDR_WIDTH = 2;
     parameter PRBS31_ENABLE = 0;
-    parameter SCRAMBLER_DISABLE = 1;
+    parameter SCRAMBLER_DISABLE = 0;
     parameter BIT_REVERSE = 0;
     parameter TX_SERDES_PIPELINE = 0;
     parameter RX_SERDES_PIPELINE = 0;
     parameter BITSLIP_HIGH_CYCLES = 1;
     parameter BITSLIP_LOW_CYCLES = 8;
-    parameter COUNT_125US = 125000/6.4;
-
-    // Definición de señales
+    parameter COUNT_125US = 125;
+    
+    // Definicion de senales
     reg rx_clk, rx_rst, tx_clk, tx_rst;
     reg [DATA_WIDTH-1:0] xgmii_txd;
     reg [CTRL_WIDTH-1:0] xgmii_txc;
@@ -31,13 +34,105 @@ module eth_phy_10g_LL1R;
     wire [6:0] rx_error_count;
     wire rx_bad_block, rx_sequence_error, rx_block_lock, rx_high_ber, rx_status;
     reg cfg_tx_prbs31_enable, cfg_rx_prbs31_enable;
+    
+    // Generacion de clock
+    always #0.5 rx_clk = ~rx_clk;
+    always #0.5 tx_clk = ~tx_clk;
+    
+    // Loopback
+    always @ (posedge tx_clk) begin
+        if (!tx_rst) begin
+            serdes_rx_data <= serdes_tx_data;
+            serdes_rx_hdr <= serdes_tx_hdr;
+        end
+    end
+    
+    // Patrones de prueba
+    reg [63:0] test_patterns [0:5];
+    initial begin
+        test_patterns[0] = 64'hFFFFFFFFFFFFFFFF; // Todos 1
+        test_patterns[1] = 64'h0000000000000000; // Todos 0
+        test_patterns[2] = 64'h5555555555555555; // Alternar 01s
+        test_patterns[3] = 64'hAAAAAAAAAAAAAAAA; // Alternar 10s
+        test_patterns[4] = 64'hFEFEFEFEFEFEFEFE; // Todos Error
+        test_patterns[5] = 64'h0707070707070707; // Todos Idle
+    end
+    
+    integer i;
 
-    // Instanciación del módulo bajo prueba
+    always @(posedge tx_clk) begin
+        if (!tx_rst) begin
+            for (i = 0; i < 6; i = i + 1) begin
+                xgmii_txd <= test_patterns[i];
+                #100;
+            end
+        end
+    end
+    /*
+    // Aviso de 125 us (tiempo del watchdog)
+    always @* begin
+        if(dut.eth_phy_10g_rx_inst.eth_phy_10g_rx_if_inst.eth_phy_10g_rx_watchdog_inst.time_count_reg == 0)
+            $display("%0t ps: 125 us", $realtime);
+    end
+    */
+    // Testbench
+    initial begin
+        $dumpfile("eth_phy_10g_LL1R.vcd");
+        $dumpvars(0, eth_phy_10g_LL1R);
+    
+        // Monitoreo
+        $display("Ejecutando simulacion...");
+    
+        // Configurar generacion de PRBS31
+        if(PRBS31_ENABLE) begin
+            cfg_tx_prbs31_enable = 1;
+            cfg_rx_prbs31_enable = 1;
+        end else begin
+            cfg_tx_prbs31_enable = 0;
+            cfg_rx_prbs31_enable = 0;
+        end
+    
+        // Inicializar clock y reset
+        rx_clk = 0;
+        tx_clk = 0;
+        rx_rst = 1;
+        tx_rst = 1;
+        
+        // Inicializar XGMII
+        xgmii_txd = {test_patterns[0]};
+        xgmii_txc = 8'hFF;
+        
+        #10
+        rx_rst = 0;
+        tx_rst = 0;
+        
+        #2400;
+        
+    /*
+        // Forzar errores de header cada cierto tiempo
+        integer j;
+        for (j = 0; j < 200; j = j + 1) begin 
+            serdes_rx_hdr = 2'b00;
+            #40;
+        end
+    */
+	    $display("Finalizado. Tiempo de ejecucion: %0t ps", $realtime);
+        $finish;
+    end
+    
+    // Instanciacion del modulo bajo prueba
     eth_phy_10g #(
         .DATA_WIDTH(DATA_WIDTH),
         .CTRL_WIDTH(CTRL_WIDTH),
         .HDR_WIDTH(HDR_WIDTH),
-        .PRBS31_ENABLE(PRBS31_ENABLE)
+        .PRBS31_ENABLE(PRBS31_ENABLE),
+        .SCRAMBLER_DISABLE(SCRAMBLER_DISABLE),
+        .BIT_REVERSE(BIT_REVERSE),
+        .TX_SERDES_PIPELINE(TX_SERDES_PIPELINE),
+        .RX_SERDES_PIPELINE(RX_SERDES_PIPELINE),
+        .BITSLIP_HIGH_CYCLES(BITSLIP_HIGH_CYCLES),
+        .BITSLIP_LOW_CYCLES(BITSLIP_LOW_CYCLES),
+        .COUNT_125US(COUNT_125US)
     ) dut (
         .rx_clk(rx_clk),
         .rx_rst(rx_rst),
@@ -63,80 +158,5 @@ module eth_phy_10g_LL1R;
         .cfg_tx_prbs31_enable(cfg_tx_prbs31_enable),
         .cfg_rx_prbs31_enable(cfg_rx_prbs31_enable)
     );
-
-    // Generacion de clock
-    always #5 rx_clk = ~rx_clk;
-    always #5 tx_clk = ~tx_clk;
-    
-    // Loopback
-    always @ (posedge tx_clk) begin
-        if (!tx_rst) begin
-            serdes_rx_data <= serdes_tx_data;
-	        serdes_rx_hdr <= serdes_tx_hdr;
-        end
-    end
-
-    // Patrones de prueba
-    reg [63:0] test_patterns [0:5];
-    initial begin
-        test_patterns[0] = 64'hFFFFFFFFFFFFFFFF; // Todos 1
-        test_patterns[1] = 64'h0000000000000000; // Todos 0
-        test_patterns[2] = 64'h5555555555555555; // Alternar 01s
-        test_patterns[3] = 64'hAAAAAAAAAAAAAAAA; // Alternar 10s
-        test_patterns[4] = 64'hFEFEFEFEFEFEFEFE; // Todos Error
-        test_patterns[5] = 64'h0707070707070707; // Todos Idle
-    end
-    
-    integer i;
-    integer j;
-
-    // Alternar XGMII con los distintos patrones
-    always @(posedge tx_clk) begin
-        if (!tx_rst) begin
-            for (i = 0; i < 6; i = i + 1) begin
-                xgmii_txd <= test_patterns[i];
-                #100;
-            end
-        end
-    end
-    
-    // Testbench
-    initial begin
-        $dumpfile("eth_phy_10g_LL1R.vcd");
-        $dumpvars(0, eth_phy_10g_LL1R);
-
-        // Monitoreo
-        $display("Ejecutando simulacion...");
-	    $display("time\t xgmii_txd\t\t xgmii_rxd\t\t serdes_tx_data\t\t serdes_rx_data\t\t tx_hdr\t rx_hdr");
-
-        // Configurar generación de PRBS31 para transmisión y recepción
-        if(PRBS31_ENABLE) begin
-            cfg_tx_prbs31_enable = 1;
-            cfg_rx_prbs31_enable = 1;
-        end else begin
-            cfg_tx_prbs31_enable = 0;
-            cfg_rx_prbs31_enable = 0;
-        end
-
-        // Inicializar clock y reset
-        rx_clk = 0;
-        tx_clk = 0;
-        rx_rst = 1;
-        tx_rst = 1;
-    	
-        // Inicializar XGMII
-        xgmii_txd = {test_patterns[0]};
-        xgmii_txc = 8'h00;
-
-        #10
-        rx_rst = 0;
-        tx_rst = 0;
-
-	// Imprimir cada ciclo de clock
-	$monitor("%g\t %h\t %h\t %h\t %h\t %b\t %b", $time, xgmii_txd, xgmii_rxd, serdes_tx_data, serdes_rx_data, serdes_tx_hdr, serdes_rx_hdr);
-        #1000;
-
-        $finish;
-    end
 
 endmodule
