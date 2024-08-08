d7fe390c3c55edf2fe12d579a56a83132c91cdab
Rubén Zúñiga <144453288+Ruben-Zuniga@users.noreply.github.com>
2024-08-08 16:12:29 -0300

    Update frame aligner testbench

--- tb/eth_phy_10g_rx_frame_aligner_tb.v
+++ tb/eth_phy_10g_rx_frame_aligner_tb.v
@@ -1,8 +1,8 @@
-`include "eth_phy_10g_rx_frame_aligner.v"
-
-`resetall
+`include "ethernet/eth_phy_10g_rx_frame_aligner_v3.v"
 `timescale 1us / 100ns
-`default_nettype none
+
+// iverilog -o ethernet/tb/eth_phy_10g_rx_frame_aligner_tb ethernet/tb/eth_phy_10g_rx_frame_aligner_tb.v
+// vvp ethernet/tb/eth_phy_10g_rx_frame_aligner_tb
 
 module eth_phy_10g_rx_frame_aligner_tb;
 
@@ -10,29 +10,32 @@ module eth_phy_10g_rx_frame_aligner_tb;
     // Parametros del modulo
     //----------------------------
     
-    parameter  DATA_WIDTH            = 64;
-    parameter  HDR_WIDTH             = 2;
-    parameter  RX_WIDTH              = DATA_WIDTH+HDR_WIDTH;
-    parameter  BITSLIP_HIGH_CYCLES   = 1;
-    parameter  BITSLIP_LOW_CYCLES    = 8;
-    localparam SH_POS_WIDTH          = $clog2(RX_WIDTH);
+    parameter   DATA_WIDTH      = 64                        ;
+    parameter   HDR_WIDTH       = 2                         ;
+    parameter   FRAME_WIDTH     = DATA_WIDTH + HDR_WIDTH    ;
+    parameter   SH_POS_WIDTH    = $clog2(FRAME_WIDTH)       ;
     
     //----------------------------
     // Definicion de senales
     //----------------------------
     
-    reg                         clk;
-    reg                         rst;
-    reg     [RX_WIDTH-1:0]      serdes_rx;
-    reg                         serdes_rx_bitslip;
-    wire    [DATA_WIDTH-1:0]    serdes_rx_data;
-    wire    [HDR_WIDTH-1:0]     serdes_rx_hdr;
+    reg                         clk                 ;
+    reg                         rst                 ;
+
+    // Interfaz Serdes
+    reg     [FRAME_WIDTH-1:0]   i_serdes_rx         ;
+    wire                        o_serdes_rx_bitslip ;
+    wire    [DATA_WIDTH-1:0]    o_serdes_rx_data    ;
+    wire    [HDR_WIDTH-1:0]     o_serdes_rx_hdr     ;
+
+    // Status
+    wire                        o_rx_block_lock     ;
     
     //----------------------------
-    // Otras senales/variables
+    // Otras seniales/variables
     //----------------------------
     
-    reg     [SH_POS_WIDTH-1:0]  sh_pos;
+    reg     [SH_POS_WIDTH-1:0]  sh_pos              ;
     
     //----------------------------
     // Generacion de clock
@@ -57,22 +60,22 @@ module eth_phy_10g_rx_frame_aligner_tb;
     
     `ifdef FIXED_DATA
         
-        reg [RX_WIDTH-1:0] test_pattern [5:0];
+        reg [FRAME_WIDTH-1:0] test_pattern [5:0];
     
         initial begin
-            test_pattern[0] = 66'hFFFFFFFFFFFFFFFF; // Todos 1
-            test_pattern[1] = 66'h0000000000000000; // Todos 0
-            test_pattern[2] = 66'h5555555555555555; // Alternar 01s
-            test_pattern[3] = 66'hAAAAAAAAAAAAAAAA; // Alternar 10s
-            test_pattern[4] = 66'hFEFEFEFEFEFEFEFE; // Todos Error
-            test_pattern[5] = 66'h0707070707070707; // Todos Idle
+            test_pattern[0] = 66'hFFFFFFFFFFFFFFFFF; // Todos 1
+            test_pattern[1] = 66'h00000000000000000; // Todos 0
+            test_pattern[2] = 66'h55555555555555555; // Alternar 01s
+            test_pattern[3] = 66'hAAAAAAAAAAAAAAAAA; // Alternar 10s
+            test_pattern[4] = 66'hEFEFEFEFEFEFEFEFE; // Todos Error
+            test_pattern[5] = 66'h70707070707070707; // Todos Idle
         end
         
         // Asignar patrones a la entrada
         always @(posedge clk) begin
             if (!rst) begin
                 for (j = 0; j < 6; j = j + 1) begin
-                    serdes_rx <= test_pattern[j];
+                    i_serdes_rx <= test_pattern[j];
                     #100;
                 end
             end
@@ -83,7 +86,7 @@ module eth_phy_10g_rx_frame_aligner_tb;
         integer seed1 = 32'd1;
         integer seed2 = 32'd2;
         integer seed3 = 32'd3;
-        reg [RX_WIDTH-1:0] test_pattern;
+        reg [FRAME_WIDTH-1:0] test_pattern;
        
         always begin
             test_pattern = {{$dist_uniform(seed3, 0, 4), $random(seed2), $random(seed1)}};
@@ -92,16 +95,14 @@ module eth_phy_10g_rx_frame_aligner_tb;
         
         // Asignar patrones a la entrada
         always @(posedge clk) begin
-            if (!rst) begin
-                serdes_rx <= test_pattern;
-                #100;
-            end
+            if (!rst)
+                i_serdes_rx <= test_pattern;
         end
         
     `endif
     
     //----------------------------
-    // Senales internas
+    // Seniales internas
     //----------------------------
     
     always @(posedge clk) begin
@@ -113,7 +114,7 @@ module eth_phy_10g_rx_frame_aligner_tb;
     //----------------------------
     
     initial begin
-        $dumpfile("eth_phy_10g_rx_frame_aligner_tb.vcd");
+        $dumpfile("ethernet/tb/eth_phy_10g_rx_frame_aligner_tb.vcd");
         $dumpvars(0, eth_phy_10g_rx_frame_aligner_tb);
     
         $display("Ejecutando simulacion...");
@@ -122,27 +123,14 @@ module eth_phy_10g_rx_frame_aligner_tb;
         clk = 0;
         rst = 1;
         
-        // Inicializar serdes y bitslip
-        serdes_rx           = test_pattern[0];
-        serdes_rx_bitslip   = 1'b0;
+        // Inicializar serdes
+        i_serdes_rx           = {FRAME_WIDTH{1'b0}};
         
         #10;
         @(posedge clk);
         rst = 0;
-
-        #500;
-        @(posedge clk);
-        serdes_rx_bitslip = 1'b1;
-        @(posedge clk);
-        serdes_rx_bitslip = 1'b0;
         
-        #250;
-        @(posedge clk);
-        serdes_rx_bitslip = 1'b1;
-        @(posedge clk);
-        serdes_rx_bitslip = 1'b0;
-        
-        #100;
+        #1000;
         $display("Tiempo de ejecucion: %0t ps", $realtime);
         $finish;
     end
@@ -152,18 +140,17 @@ module eth_phy_10g_rx_frame_aligner_tb;
     //----------------------------
     
     eth_phy_10g_rx_frame_aligner #(
-        .DATA_WIDTH(DATA_WIDTH),
-        .HDR_WIDTH(HDR_WIDTH),
-        .RX_WIDTH(RX_WIDTH),
-        .BITSLIP_HIGH_CYCLES(BITSLIP_HIGH_CYCLES),
-        .BITSLIP_LOW_CYCLES(BITSLIP_LOW_CYCLES)
+        .DATA_WIDTH             (DATA_WIDTH)            ,
+        .HDR_WIDTH              (HDR_WIDTH)             ,
+        .FRAME_WIDTH            (FRAME_WIDTH)           
     ) dut (
-        .clk(clk),
-        .rst(rst),
-        .serdes_rx(serdes_rx),
-        .serdes_rx_bitslip(serdes_rx_bitslip),
-        .serdes_rx_data(serdes_rx_data),
-        .serdes_rx_hdr(serdes_rx_hdr)
+        .clk                    (clk)                   ,
+        .rst                    (rst)                   ,
+        .i_serdes_rx            (i_serdes_rx)           ,
+        .o_serdes_rx_bitslip    (o_serdes_rx_bitslip)   ,
+        .o_serdes_rx_data       (o_serdes_rx_data)      ,
+        .o_serdes_rx_hdr        (o_serdes_rx_hdr)       ,
+        .o_rx_block_lock        (o_rx_block_lock)
     );
 
 endmodule
